a530ba95654d8d49f541704771bca3bb27fad313
PlaIsMe <phongvulai96@gmail.com>
2025-06-29 15:41:15 +0700

    Refactor code block item init

--- src/main/java/com/pla/pladailyboss/init/BlockInit.java
+++ src/main/java/com/pla/pladailyboss/init/BlockInit.java
@@ -1,37 +1,17 @@
 package com.pla.pladailyboss.init;
 
 import com.pla.pladailyboss.PlaDailyBoss;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
-import java.util.function.Supplier;
 
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, PlaDailyBoss.MOD_ID);
 
-    public static final RegistryObject<Block> SPINNING_BLOCK = registerBlock("spinning_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
-        return toReturn;
-    }
-
-    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
-        return ItemInit.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-    }
-
-    public static void register(IEventBus eventBus) {
-        BLOCKS.register(eventBus);
-    }
+    public static final RegistryObject<Block> SPINNING_BLOCK = BLOCKS.register("spinning_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.BEDROCK)));
 }
--- src/main/java/com/pla/pladailyboss/init/ItemInit.java
+++ src/main/java/com/pla/pladailyboss/init/ItemInit.java
@@ -2,14 +2,15 @@ package com.pla.pladailyboss.init;
 
 import com.pla.pladailyboss.PlaDailyBoss;
 import net.minecraft.world.item.*;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PlaDailyBoss.MOD_ID);
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+
+    public static final RegistryObject<BlockItem> SPINNING_BLOCK_ITEM = ITEMS.register("spinning_block",
+            () -> new BlockItem(BlockInit.SPINNING_BLOCK.get(),
+                    new Item.Properties()
+                            .rarity(Rarity.UNCOMMON)));
 }
