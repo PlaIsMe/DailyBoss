b8cf1b65d92baf656d4c0d179642a1c08d5e409a
PlaIsMe <phongvulai96@gmail.com>
2025-07-04 20:52:10 +0700

    Enhance UI

--- src/main/java/com/pla/pladailyboss/client/screen/BossScreen.java
+++ src/main/java/com/pla/pladailyboss/client/screen/BossScreen.java
@@ -23,29 +23,33 @@ import java.util.List;
 public class BossScreen extends Screen {
     private static final Logger LOGGER = LogManager.getLogger();
     private static BossScreen instance;
-    private static final Component TITLE =
-            Component.translatable("gui." + PlaDailyBoss.MOD_ID + ".boss_screen");
 
     private static final String posterPath = "textures/gui/entity_posters/";
 
+    private static final ResourceLocation TITLE =
+            ResourceLocation.fromNamespaceAndPath(PlaDailyBoss.MOD_ID, "textures/gui/element/title.png");
+
     private static final ResourceLocation BACKGROUND =
-            ResourceLocation.fromNamespaceAndPath(PlaDailyBoss.MOD_ID, "textures/gui/screen_background.png");
+            ResourceLocation.fromNamespaceAndPath(PlaDailyBoss.MOD_ID, "textures/gui/element/screen_background.png");
+
+    private static final ResourceLocation FRAME =
+            ResourceLocation.fromNamespaceAndPath(PlaDailyBoss.MOD_ID, "textures/gui/element/frame.png");
 
     private int currentPage;
     private int totalPages;
     private Button pageButton;
     private List<BossEntry> entityIdStrings;
 
-    private static final int BOX_WIDTH = 60;
-    private static final int BOX_HEIGHT = 80;
+    private static final int BOX_WIDTH = 90;
+    private static final int BOX_HEIGHT = 120;
     private static final int GUI_WIDTH = 512;
     private static final int GUI_HEIGHT = 320;
-    private static final int PADDING_HORIZONTAL = 5;
-    private static final int PADDING_VERTICAL = 4;
+    private static final int PADDING_HORIZONTAL = 20;
+    private static final int PADDING_VERTICAL = 5;
     private static final int BUTTON_SIZE = 20;
 
     public BossScreen() {
-        super(TITLE);
+        super(Component.translatable("gui." + PlaDailyBoss.MOD_ID + ".boss_screen"));
         instance = this;
         NetworkHandler.INSTANCE.sendToServer(new AskForDataMessage());
     }
@@ -78,17 +82,8 @@ public class BossScreen extends Screen {
         currentPage = Math.max(1, Math.min(currentPage, totalPages));
 
         addRenderableWidget(new Button(
-                bgLeft + (bgWidth - BUTTON_SIZE * 5) / 2,
-                bgTop - BUTTON_SIZE,
-                BUTTON_SIZE * 5,
-                BUTTON_SIZE,
-                Component.literal("Daily Boss List"),
-                this::doNothing
-        ));
-
-        addRenderableWidget(new Button(
-                bgRight - BUTTON_SIZE * 4 - PADDING_HORIZONTAL,
-                bgBottom + PADDING_VERTICAL / 2,
+                bgRight - BUTTON_SIZE * 4 - PADDING_HORIZONTAL - 30,
+                bgBottom - 30,
                 BUTTON_SIZE,
                 BUTTON_SIZE,
                 Component.literal("<"),
@@ -96,8 +91,8 @@ public class BossScreen extends Screen {
         ));
 
         pageButton = addRenderableWidget(new Button(
-                bgRight - BUTTON_SIZE * 3 - PADDING_HORIZONTAL,
-                bgBottom + PADDING_VERTICAL / 2,
+                bgRight - BUTTON_SIZE * 3 - PADDING_HORIZONTAL- 30,
+                bgBottom - 30,
                 BUTTON_SIZE * 2,
                 BUTTON_SIZE,
                 Component.literal(currentPage + "/" + totalPages),
@@ -105,8 +100,8 @@ public class BossScreen extends Screen {
         ));
 
         addRenderableWidget(new Button(
-                bgRight - BUTTON_SIZE - PADDING_HORIZONTAL,
-                bgBottom + PADDING_VERTICAL / 2,
+                bgRight - BUTTON_SIZE - PADDING_HORIZONTAL- 30,
+                bgBottom - 30,
                 BUTTON_SIZE,
                 BUTTON_SIZE,
                 Component.literal(">"),
@@ -126,14 +121,15 @@ public class BossScreen extends Screen {
         if (!textureExists(entityCardTexture)) {
             entityCardTexture = ResourceLocation.fromNamespaceAndPath(PlaDailyBoss.MOD_ID, posterPath + "not_found/not_found" + prefixImage);
         }
+        int imagePadding = 5;
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderTexture(0, entityCardTexture);
-        this.blit(
+        blit(
                 poseStack,
-                x, y,
+                x + imagePadding, y + imagePadding,
                 0, 0,
-                BOX_WIDTH, BOX_HEIGHT,
-                BOX_WIDTH, BOX_HEIGHT
+                BOX_WIDTH - imagePadding * 2, BOX_HEIGHT - imagePadding * 2,
+                BOX_WIDTH - imagePadding * 2, BOX_HEIGHT - imagePadding * 2
         );
     }
 
@@ -147,9 +143,20 @@ public class BossScreen extends Screen {
 
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderTexture(0, BACKGROUND);
-        this.blit(poseStack, bgLeft, bgTop, 0, 0, bgWidth, bgHeight, bgWidth, bgHeight);
+        blit(poseStack, bgLeft, bgTop, 0, 0, bgWidth, bgHeight, bgWidth, bgHeight);
 
         super.render(poseStack, mouseX, mouseY, partialTicks);
+        int titleWidth = 100;
+        int titleHeight = 18;
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, TITLE);
+        blit(
+                poseStack,
+                bgLeft + (GUI_WIDTH - titleWidth) / 2, bgTop + 22,
+                0, 0,
+                titleWidth, titleHeight,
+                titleWidth, titleHeight
+        );
 
         if (this.entityIdStrings == null) return;
 
@@ -160,8 +167,7 @@ public class BossScreen extends Screen {
 
         int usableHeight = GUI_HEIGHT - 2 * PADDING_VERTICAL;
         int maxRows = usableHeight / (BOX_HEIGHT + PADDING_VERTICAL);
-        int totalHeight = maxRows * BOX_HEIGHT + (maxRows - 1) * PADDING_VERTICAL;
-        int startY = bgTop + (GUI_HEIGHT - totalHeight) / 2;
+        int startY = bgTop + 40;
 
         int entitiesPerPage = maxRows * maxColumns;
         totalPages = Math.max(1, (int) Math.ceil(entityIdStrings.size() / (double) entitiesPerPage));
@@ -175,7 +181,7 @@ public class BossScreen extends Screen {
             int row = i / maxColumns;
 
             int x = startX + col * (BOX_WIDTH + PADDING_HORIZONTAL);
-            int y = startY + row * (BOX_HEIGHT + PADDING_HORIZONTAL);
+            int y = startY + row * (BOX_HEIGHT + PADDING_VERTICAL);
 
             drawEntityCard(poseStack, x, y, entitiesToRender.get(i));
         }
