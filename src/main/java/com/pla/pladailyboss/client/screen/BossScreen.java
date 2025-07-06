984f73fbbad3de0772aea8300484f73e28e63124
PlaIsMe <phongvulai96@gmail.com>
2025-07-06 11:51:49 +0700

    Remove redundant import

--- src/main/java/com/pla/pladailyboss/client/screen/BossScreen.java
+++ src/main/java/com/pla/pladailyboss/client/screen/BossScreen.java
@@ -12,7 +12,6 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.world.entity.monster.Evoker;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
