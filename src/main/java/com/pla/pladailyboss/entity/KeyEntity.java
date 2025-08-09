f43673c008e9dfa408813efe9fb0e0d62e92c920
PlaIsMe <phongvulai96@gmail.com>
2025-08-09 12:45:25 +0700

    Update 1.4: Bosses Rise compat

--- src/main/java/com/pla/pladailyboss/entity/KeyEntity.java
+++ src/main/java/com/pla/pladailyboss/entity/KeyEntity.java
@@ -30,6 +30,9 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
+import net.unusual.blockfactorysbosses.entity.InfernalDragonEntity;
+import net.unusual.blockfactorysbosses.entity.SandwormEntity;
+import net.unusual.blockfactorysbosses.entity.UnderworldKnightEntity;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -269,6 +272,15 @@ public class KeyEntity extends Mob {
                     mob.setPos(this.getX(), this.getY(), this.getZ());
                     mob.setPersistenceRequired();
                     mob.setTarget(player);
+                    if (Objects.equals(selectedMobId, "block_factorys_bosses:sandworm")) {
+                        mob.getEntityData().set(SandwormEntity.DATA_spawn_animtime, 180);
+                    }
+                    if (Objects.equals(selectedMobId, "block_factorys_bosses:infernal_dragon")) {
+                        mob.getEntityData().set(InfernalDragonEntity.DATA_spawn_animtime, 236);
+                    }
+                    if (Objects.equals(selectedMobId, "block_factorys_bosses:underworld_knight")) {
+                        mob.getEntityData().set(UnderworldKnightEntity.DATA_spawn_animtime, 226);
+                    }
                     level().addFreshEntity(mob);
                 }
 
