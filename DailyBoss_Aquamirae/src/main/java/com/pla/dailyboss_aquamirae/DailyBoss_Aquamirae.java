cc9914f5b3a52c72b37bfe3a2aa8913a3342cc0b
PlaIsMe <phongvulai96@gmail.com>
2025-11-08 10:55:37 +0700

    Add Aquamire Compat

---
+++ DailyBoss_Aquamirae/src/main/java/com/pla/dailyboss_aquamirae/DailyBoss_Aquamirae.java
@@ -0,0 +1,37 @@
+package com.pla.dailyboss_aquamirae;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(DailyBoss_Aquamirae.MOD_ID)
+public class DailyBoss_Aquamirae
+{
+    public static final String MOD_ID = "dailyboss_aquamirae";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DailyBoss_Aquamirae() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
