2fe2044ddf7bd66b1a9c95ebb324d0a859a8ddec
PlaIsMe <phongvulai96@gmail.com>
2025-11-09 20:11:49 +0700

    Add Cataclysm compat

---
+++ DailyBoss_Cataclysm/src/main/java/com/pla/dailyboss_cataclysm/DailyBoss_Cataclysm.java
@@ -0,0 +1,37 @@
+package com.pla.dailyboss_cataclysm;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(DailyBoss_Cataclysm.MOD_ID)
+public class DailyBoss_Cataclysm
+{
+    public static final String MOD_ID = "dailyboss_cataclysm";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DailyBoss_Cataclysm() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
